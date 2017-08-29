8546b906be852ee505d270b2b7071d56199b56a2
clarkgh <demolisho@hotmail.com>
2017-08-29 13:12:28 -0700

    tutorial 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,12 +25,22 @@ const HEROES: Hero[] = [
   styleUrls: ['./app.component.css'],
   template: `
     <h1>{{title}}</h1>
-    <h2>Mr Heroes</h2>
+    <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      <li *ngFor="let hero of heroes" 
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+          <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
     `,
   styles: [`
   .selected {
@@ -86,8 +96,8 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
